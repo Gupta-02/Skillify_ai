0f7fc32a31a8aa1973b1fc6a685fb9329463c89b
Gupta-02 <Gupta-02@users.noreply.github.com>
2025-10-21 16:38:51 +0530

    Style: Update global styles and root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,49 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import localFont from "next/font/local";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import Navbar from "@/components/Navbar";
+import { Providers } from "@/components/providers";
+import { Footer } from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Analytics } from "@vercel/analytics/react";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
-  title: 'Test.AI - AI-Powered Testing Platform',
-  description: 'Create, manage, and evaluate assessments with AI',
-}
+  title: "Test.ai",
+  description: "AI-Powered Quizzes Tailored to Boost Your Knowledge",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} font-sans dark:bg-black`}
+      >
+        <Analytics />
+        <Providers>
+          <Toaster />
+          <header className="flex-none w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <Navbar />
+          </header>
+          <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 dark:bg-black h-screen font-sans">
+            {children}
+          </main>
+          {/* <footer className="bottom left-0 w-full"> */}
+          {/*   <Footer /> */}
+          {/* </footer> */}
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
